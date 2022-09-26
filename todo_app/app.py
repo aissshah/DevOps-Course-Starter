39c41b8969402bd516beca6f6c5da5181eff86fe
Aishah Omar <Aishah.Omar@softwire.com>
2022-09-26 17:17:39 +0100

    Step 3: create new to-do items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 from todo_app.flask_config import Config
-from todo_app.data.session_items import get_items
+from todo_app.data.session_items import get_items, add_item
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -11,3 +11,9 @@ app.config.from_object(Config())
 def index():
     items = get_items()
     return render_template('index.html', items=items)
+
+@app.route('/create-item', methods=['POST'])
+def create_item():
+    todo = request.form.get('todo')
+    add_item(todo)
+    return redirect('/')
