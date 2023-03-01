bab9b5f79f58e76e46150528042f23e6a9e33c73
Aishah Omar <Aishah.Omar@softwire.com>
2023-03-01 10:45:33 +0000

    M2P5: create a class for to-do items

--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -1,14 +1,28 @@
 import requests
 import os
 
+from todo_app.item import Item
+
 auth = {
       'key': os.getenv('TRELLO_KEY'), 
-      'token': os.getenv('TRELLO_token'),
+      'token': os.getenv('TRELLO_TOKEN'),
     }
 
 def get_items():
     response = requests.get('https://trello.com/1/boards/{board}/cards'.format(board=os.getenv('TRELLO_BOARD')), params=auth)
-    return response.json()
+    cards = response.json()
+    items = []
+    for card in cards:
+        item = Item.from_trello_card(card, map_list_id_to_name(card['idList']))
+        items.append(item)
+    return items
+
+def map_list_id_to_name(listID):
+    lists = get_lists()
+    for list in lists:
+        if listID == list['id']:
+            return list['name']
+    return ''
 
 def add_item(title):
     boardLists = get_lists()
@@ -18,12 +32,8 @@ def add_item(title):
     response = requests.post('https://trello.com/1/cards', params=newParams)
     return response.json()
 
-def update_item_status(item):
-    return ''
-
 def get_lists():
     response = requests.get('https://trello.com/1/boards/{board}/lists'.format(board=os.getenv('TRELLO_BOARD')), params=auth)
-    print(response.status_code, response.reason)
     return response.json()
 
 def find_list_by_name(listName, boardList):
