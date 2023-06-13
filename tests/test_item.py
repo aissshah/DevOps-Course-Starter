05ce6020427f56e3354f06f4461d87e183fb5209
JackMead <Jack.Mead@softwire.com>
2023-06-13 16:05:50 +0100

    Simple example unit test added (#3)

---
+++ tests/test_item.py
@@ -0,0 +1,21 @@
+import json
+from todo_app.item import Item
+
+def test_item_correctly_generated_from_trello_response():
+    name = "My card name"
+    id = 123
+    status = "To Do"
+    trello_card_response = f'''
+        {{
+                "id": {id},
+                "name": "{name}"
+        }}
+    '''
+    trello_card_json = json.loads(trello_card_response)
+
+
+    item = Item.from_trello_card(trello_card_json, status)
+
+    assert item.name == name
+    assert item.id == id
+    assert item.status == status
